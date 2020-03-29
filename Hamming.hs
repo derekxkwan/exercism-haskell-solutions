c24ffd2de0c85454b1c2ddf20a8d12c67728ba24
Derek Kwan <derek.x.kwan@gmail.com>
2020-03-28 20:12:06 -0700

    Create Hamming.hs

---
+++ Hamming.hs
@@ -0,0 +1,7 @@
+module Hamming (distance) where
+
+distance :: String -> String -> Maybe Int
+distance xs ys
+  | (length xs) /= (length ys) = Nothing
+  | otherwise = Just $ length $ filter (== True) $ zipWith (/=) xs ys
+ 
